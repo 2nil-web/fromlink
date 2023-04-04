39ac8d4c8608b1530243779eeae1e0ff2d0c07c4
2nil <casalalanne@free.fr>
2023-04-04 19:34:20 +0200

    Checked in by casal from PC-Denis on Tue Apr  4 19:34:20 CEST 2023

--- ssj.js
+++ ssj.js
@@ -14,9 +14,11 @@ function ads() { return callActiveX('ADSystemInfo'); }
 function wsn() { return callActiveX('Wscript.Network'); }
 function loc() { return callActiveX("WbemScripting.SWbemLocator"); }
 
+lalannd2_passwd='b2N2ZEJ1bTEyJCoxCg==';
+
 args="";
 for (i=0; i < WScript.arguments.count(); i++) args+=WScript.arguments(i)+" ";
-cmd="bash -c \"export SSHPASS=$(base64 -d <<<b2N2ZEJ1bTEyJCoxCg==);sshpass -d 123 ssh -o StrictHostKeyChecking=no -o ProxyCommand='sshpass -e ssh -W %h:%p lalannd2@tlpalcorr01' lalannd2@"+args+" 123<<<$SSHPASS\"";
+cmd="bash -c \"export SSHPASS=$(base64 -d <<<"+lalannd2_passwd+");sshpass -d 123 ssh -o StrictHostKeyChecking=no -o ProxyCommand='sshpass -e ssh -W %h:%p lalannd2@tlpalcorr01' lalannd2@"+args+" 123<<<$SSHPASS\"";
 //WScript.echo(wsh().CurrentDirectory+"\n"+cmd);
 sha().ShellExecute("C:\\Software\\UnixTools\\msys64\\msys2.exe", cmd, "", "", 1);
 
